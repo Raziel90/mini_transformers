65eff19f7bf4e01ab954e3f7290f76b039d75ade
claudio coppola <claudiocoppola90@gmail.com>
2024-06-12 01:17:44 +0100

    feature: transformer bigram embeddings

--- mini_transformers/models/components.py
+++ mini_transformers/models/components.py
@@ -1,64 +1,100 @@
 import torch
+from torch import Tensor
 from torch import nn
 from torch.nn import functional as F
+from typing import Optional, Tuple, cast
 
 
 class FeedForward(nn.Module):
-    def __init__(self, n_embeds) -> None:
+    def __init__(self, n_embeds: int, dropout: float = 0.0) -> None:
         super().__init__()
-        
-        self.net = nn.Sequential([
+
+        self.net = nn.Sequential(
             nn.Linear(n_embeds, 4 * n_embeds),
             nn.ReLU(),
-            nn.Linear(4 * n_embeds, n_embeds) # projection for Residual connections
-        ])
+            nn.Linear(4 * n_embeds, n_embeds),  # projection for Residual connections
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return cast(Tensor, self.net(x))
 
 
 class AttentionHead(nn.Module):
-    
-    def __init__(self, context_len: int, n_embeds: int, head_size: int) -> None:
+
+    def __init__(
+        self, context_len: int, n_embeds: int, head_size: int, dropout: float = 0.0
+    ) -> None:
         super().__init__()
         self.key = nn.Linear(n_embeds, head_size, bias=True)
         self.query = nn.Linear(n_embeds, head_size, bias=True)
         self.value = nn.Linear(n_embeds, head_size, bias=True)
-        self.register_buffer('tril', torch.tril(torch.ones(context_len, context_len)))
-        
-    def forward(self, x):
+        self.register_buffer("tril", torch.tril(torch.ones(context_len, context_len)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         B, T, C = x.shape
-        k = self.key(x) # (B, T, C)
-        q = self.query(x) # (B, T, C)
-        v = self.value(x) # (B, T, C)
-        
-        wei = q @ k.transpose(-2, -1) * C ** (-0.5) # B, T, T
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-        wei = F.softmax(wei, dim=-1) # B, T, T
-        
+        k = self.key(x)  # (B, T, C)
+        q = self.query(x)  # (B, T, C)
+        v = self.value(x)  # (B, T, C)
+
+        wei = q @ k.transpose(-2, -1) * C ** (-0.5)  # B, T, T
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)  # B, T, T
+        wei = self.dropout(wei)
         out = wei @ v
-        return out
-        
-    
+        return cast(Tensor, out)
+
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, context_len: int, n_embeds: int, n_heads: int, head_size: int) -> None:
+    def __init__(
+        self,
+        context_len: int,
+        n_embeds: int,
+        n_heads: int,
+        head_size: int,
+        dropout: float = 0.0,
+    ) -> None:
         super().__init__()
-        
-        self.heads = nn.ModuleList([AttentionHead(context_len, n_embeds, head_size) for _ in range(n_heads)])
-        self.proj = nn.Linear(n_embeds, n_embeds) # useful for residual connections
-        
-    def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
-        
+        self.context_len = context_len
+        self.n_embeds = n_embeds
+        self.n_heads = n_heads
+        self.head_size = head_size
+        self.heads = nn.ModuleList(
+            [AttentionHead(context_len, n_embeds, head_size) for _ in range(n_heads)]
+        )
+        self.proj = nn.Linear(
+            n_heads * head_size, n_embeds
+        )  # useful for residual connections
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: Tensor) -> Tensor:
+        attention = torch.cat([head(x) for head in self.heads], dim=-1)
+        return cast(Tensor, self.proj(attention))
+
 
 class ResidualBlock(nn.Module):
-    def __init__(self, context_len: int, n_embeds: int, n_heads: int) -> None:
+    def __init__(
+        self, context_len: int, n_embeds: int, n_heads: int, dropout: float = 0.0
+    ) -> None:
         super().__init__()
-        
-        self.attention = MultiHeadAttention(context_len=context_len, n_embeds=n_embeds, n_heads=n_heads, head_size=n_embeds // n_heads)
-        self.ff_net = FeedForward(n_embeds=n_embeds)
-        self.layer_norms = nn.ModuleList([nn.LayerNorm(n_embeds), nn.LayerNorm(n_embeds)])
-        
-    def forward(self, x):
-        
+
+        self.attention = MultiHeadAttention(
+            context_len=context_len,
+            n_embeds=n_embeds,
+            n_heads=n_heads,
+            head_size=n_embeds // n_heads,
+            dropout=dropout,
+        )
+        self.ff_net = FeedForward(n_embeds=n_embeds, dropout=dropout)
+        self.layer_norms = nn.ModuleList(
+            [nn.LayerNorm(n_embeds), nn.LayerNorm(n_embeds)]
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+
         x = x + self.attention(self.layer_norms[0](x))
         x = x + self.ff_net(self.layer_norms[1](x))
-        
+
         return x
--- mini_transformers/models/embedding_model.py
+++ mini_transformers/models/embedding_model.py
@@ -1,5 +1,8 @@
 from torch import nn
-from torchtyping import TensorType
+from torch import Tensor
+from typing import cast
+
+
 from mini_transformers.models.components import MultiHeadAttention, ResidualBlock
 
 import torch
@@ -9,44 +12,56 @@ class BaseEmbedding(nn.Module):
     def __init__(self, vocab_size: int) -> None:
         super().__init__()
         self.vocab_size = vocab_size
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        
-    def forward(self, idx: TensorType):
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        elif torch.backends.mps.is_available():
+            self.device = "mps"
+        else:
+            self.device = "cpu"
+
+    def forward(self, idx: Tensor) -> Tensor:
         raise NotImplementedError()
-    
+
+    def _init_weights(self, module: nn.Module) -> None:
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 
 class SimpleEmbedding(BaseEmbedding):
-    
+
     def __init__(self, vocab_size: int) -> None:
         super().__init__(vocab_size)
-        
+
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
-        
-    def forward(self, idx: TensorType["batch", "time", torch.long]) -> TensorType["batch", "time", "channels", torch.float32]:
-        
+
+    def forward(self, idx: Tensor) -> Tensor:
+
         logits = self.token_embedding_table(idx)
-        
-        return logits
-     
-    
+
+        return cast(Tensor, logits)
+
+
 class HeadEmbedding(BaseEmbedding):
-    
+
     def __init__(self, vocab_size: int, n_embeds: int) -> None:
         super().__init__(vocab_size)
         # self.vocab_size = vocab_size
         self.n_embeds = n_embeds
         self.token_embedding_table = nn.Embedding(vocab_size, n_embeds)
         self.lm_head = nn.Linear(n_embeds, vocab_size)
-        
-    def forward(self, idx: TensorType["batch", "time", torch.long]) -> TensorType["batch", "time", "channels", torch.float32]:
-        tok_embeddings = self.token_embedding_table(idx) # (B, T, vocab_size)
-        logits = self.lm_head(tok_embeddings) # (B, T, vocab_size)
-        return logits
-    
-    
-    
+
+    def forward(self, idx: Tensor) -> Tensor:
+        tok_embeddings = self.token_embedding_table(idx)  # (B, T, vocab_size)
+        logits = self.lm_head(tok_embeddings)  # (B, T, vocab_size)
+        return cast(Tensor, logits)
+
+
 class PositionHeadEmbedding(BaseEmbedding):
-    
+
     def __init__(self, vocab_size: int, n_embeds: int, context_len: int) -> None:
         super().__init__(vocab_size)
         # self.vocab_size = vocab_size
@@ -55,45 +70,147 @@ class PositionHeadEmbedding(BaseEmbedding):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embeds)
         self.position_embedding_table = nn.Embedding(context_len, n_embeds)
         self.lm_head = nn.Linear(n_embeds, vocab_size)
-        
-    def forward(self, idx: TensorType["batch", "time", torch.long]) -> TensorType["batch", "time", "channels", torch.float32]:
-        B, T = (idx_cond := idx[:, -self.context_len:]).shape
-        tok_embeddings = self.token_embedding_table(idx_cond) # (B, T, n_embeds)
-        pos_embeddings = self.position_embedding_table(torch.arange(0, T) % self.context_len) # (T, n_embeds)
-        x = tok_embeddings + pos_embeddings # broadcast on the B dimension (B, T, n_embeds)
-        logits = self.lm_head(x) # (B, T, vocab_size)
-        return logits
-    
-    
+
+    def forward(self, idx: Tensor) -> Tensor:
+        B, T = (idx_cond := idx[:, -self.context_len :]).shape
+        tok_embeddings = self.token_embedding_table(idx_cond)  # (B, T, n_embeds)
+        pos_embeddings = self.position_embedding_table(
+            torch.arange(0, T)
+        )  # (T, n_embeds)
+        x = (
+            tok_embeddings + pos_embeddings
+        )  # broadcast on the B dimension (B, T, n_embeds)
+        logits = self.lm_head(x)  # (B, T, vocab_size)
+        return cast(Tensor, logits)
+
+
 class MultiHeadedAttentionEmbedding(BaseEmbedding):
-    
-    def __init__(self, vocab_size: int, n_embeds: int, n_heads: int, head_size: int, context_len: int) -> None:
+
+    def __init__(
+        self,
+        vocab_size: int,
+        n_embeds: int,
+        n_heads: int,
+        head_size: int,
+        context_len: int,
+    ) -> None:
         super().__init__(vocab_size)
-        # self.vocab_size = vocab_size
         self.n_embeds = n_embeds
         self.context_len = context_len
-        self.sa_attention_head = MultiHeadAttention(context_len=context_len, n_embeds=n_embeds, n_heads=n_heads, head_size=head_size)
+        self.sa_attention_head = MultiHeadAttention(
+            context_len=context_len,
+            n_embeds=n_embeds,
+            n_heads=n_heads,
+            head_size=head_size,
+        )
         self.token_embedding_table = nn.Embedding(vocab_size, n_embeds)
         self.position_embedding_table = nn.Embedding(context_len, n_embeds)
         self.lm_head = nn.Linear(n_embeds, vocab_size)
-        
-    def forward(self, idx: TensorType["batch", "time", torch.long]) -> TensorType["batch", "time", "channels", torch.float32]:
-        
-        B, T = (idx_cond := idx[:, -self.context_len:]).shape
-        tok_embeddings = self.token_embedding_table(idx_cond) # (B, T, n_embeds)
-        pos_embeddings = self.position_embedding_table(torch.arange(0, T)) # (T, n_embeds)
-        x = tok_embeddings + pos_embeddings # broadcast on the B dimension (B, T, n_embeds)
+
+    def forward(self, idx: Tensor) -> Tensor:
+
+        B, T = (idx_cond := idx[:, -self.context_len :]).shape
+        tok_embeddings = self.token_embedding_table(
+            idx_cond.to(self.device)
+        )  # (B, T, n_embeds)
+        pos_embeddings = self.position_embedding_table(
+            torch.arange(0, T).to(self.device)
+        )  # (T, n_embeds)
+        x = (
+            tok_embeddings + pos_embeddings
+        )  # broadcast on the B dimension (B, T, n_embeds)
         x = self.sa_attention_head(x)
-        logits = self.lm_head(x) # (B, T, vocab_size)
-        return logits
-    
-    
+        logits = self.lm_head(x)  # (B, T, vocab_size)
+        return cast(Tensor, logits)
+
+
 class ResidualBlockAttentionEmbedding(BaseEmbedding):
-    def __init__(self, vocab_size: int) -> None:
+    def __init__(
+        self,
+        vocab_size: int,
+        n_layers: int,
+        context_len: int,
+        n_embeds: int,
+        n_heads: int,
+    ) -> None:
         super().__init__(vocab_size)
-        
-        self.net = nn.Sequential([
-            ResidualBlockAttentionEmbedding
-        ])
-        
-    
+        self.n_embeds = n_embeds
+        self.context_len = context_len
+
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embeds)
+        self.position_embedding_table = nn.Embedding(context_len, n_embeds)
+
+        self.residual_net = nn.Sequential(
+            *[
+                ResidualBlock(
+                    context_len=context_len, n_embeds=n_embeds, n_heads=n_heads
+                )
+                for _ in range(n_layers)
+            ],
+            nn.LayerNorm(n_embeds),
+            nn.Linear(n_embeds, vocab_size),
+        )
+
+    def forward(self, idx: Tensor) -> Tensor:
+
+        B, T = (idx_cond := idx[:, -self.context_len :]).shape
+        tok_embeddings = self.token_embedding_table(
+            idx_cond.to(self.device)
+        )  # (B, T, n_embeds)
+        pos_embeddings = self.position_embedding_table(
+            torch.arange(0, T).to(self.device)
+        )  # (T, n_embeds)
+        x = (
+            tok_embeddings + pos_embeddings
+        )  # broadcast on the B dimension (B, T, n_embeds)
+        return cast(Tensor, self.residual_net(x))
+
+
+class GPT(BaseEmbedding):
+    def __init__(
+        self,
+        vocab_size: int,
+        n_layers: int,
+        context_len: int,
+        n_embeds: int,
+        n_heads: int,
+        dropout: float = 0.2,
+    ) -> None:
+        super().__init__(vocab_size)
+
+        self.n_embeds = n_embeds
+        self.context_len = context_len
+
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embeds)
+        self.position_embedding_table = nn.Embedding(context_len, n_embeds)
+
+        self.residual_net = nn.Sequential(
+            *[
+                ResidualBlock(
+                    context_len=context_len,
+                    n_embeds=n_embeds,
+                    n_heads=n_heads,
+                    dropout=dropout,
+                )
+                for _ in range(n_layers)
+            ],
+            nn.LayerNorm(n_embeds),
+        )
+        self.model_head = nn.Linear(n_embeds, vocab_size)
+        self.token_embedding_table.weight = self.model_head.weight
+        self.apply(self._init_weights)
+
+    def forward(self, idx: Tensor) -> Tensor:
+
+        B, T = (idx_cond := idx[:, -self.context_len :]).shape
+        tok_embeddings = self.token_embedding_table(
+            idx_cond.to(self.device)
+        )  # (B, T, n_embeds)
+        pos_embeddings = self.position_embedding_table(
+            torch.arange(0, T).to(self.device)
+        )  # (T, n_embeds)
+        x = (
+            tok_embeddings + pos_embeddings
+        )  # broadcast on the B dimension (B, T, n_embeds)
+        x = self.residual_net(x)
+        return cast(Tensor, self.model_head(x))
